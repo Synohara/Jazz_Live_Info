cae4bd338ffaff4b009e7125093b7c02dad6fc32
Makoto Shinohara <jyoniums@gmail.com>
2019-12-17 22:22:59 +0900

    django only

--- frontend/frontend/config/test.env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const merge = require("webpack-merge");
-const devEnv = require("./dev.env");
-
-export default merge(devEnv, {
-  NODE_ENV: '"testing"'
-});
--- frontend_v2/frontend/src/api/lives.js
+++
@@ -1,8 +0,0 @@
-export default function (cli) {
-    return {
-      list (page) {
-        let offset = page * 20;
-        return cli.get(`lives/?limit=${20}&offset=${offset}`)
-      },
-    }
-  }
